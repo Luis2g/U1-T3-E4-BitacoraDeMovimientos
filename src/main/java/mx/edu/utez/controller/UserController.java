4f3c341e923270f8e6e52da789669ef75b1125fe
Vontrailop <20193tn051@utez.edu.mx>
2022-02-08 10:26:11 -0600

    [UPDATE] User
    Añadí servicio de login

--- src/main/java/mx/edu/utez/controller/UserController.java
+++ src/main/java/mx/edu/utez/controller/UserController.java
@@ -46,4 +46,9 @@ public class UserController {
 	public void delete(@PathVariable("id") long id) {
 		userService.remove(id);
 	}
+	
+	@PostMapping("/login")
+	public User login(@RequestBody User user) {
+		return userService.login(user);
+	}
 }
--- src/main/java/mx/edu/utez/repository/UserRepository.java
+++ src/main/java/mx/edu/utez/repository/UserRepository.java
@@ -2,10 +2,15 @@ package mx.edu.utez.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
 
 import mx.edu.utez.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-
+	
+	@Procedure (name = "login")
+	User login(@Param("usernameIn") String usernameIn, @Param("passwordIn") String passwordIn);
+	
 }
