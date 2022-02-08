171613ecc9501ded9c88c0d262d9dc52d975cfd6
DiegoVelascoGH <20193tn054@utez.edu.mx>
2022-02-08 16:48:46 -0600

    [UPDATE] Vista

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -5,9 +5,11 @@ app.controller('HouseController', function ($scope, $http) {
     const raiz = "http://localhost:8080/databaseLog/"
 
     $scope.user = {};
+    $scope.houseWrapper = {};
 
+
+// LOGIN
     $scope.login = () => {
-        console.log($scope.user);
         $http({
             method: 'POST',
             url: raiz + "login",
@@ -16,11 +18,77 @@ app.controller('HouseController', function ($scope, $http) {
             if(response){
                 console.log(response);
                 window.location.replace('/casas.html');
-                alert('Entraste')
             }else{
                 console.log("Sin permisos");
             }
         })
     }
+    
+    
+    // Registro de una casa
+    $scope.registrarCasa = () => {
+		console.log($scope.houseWrapper);
+		$http({
+            method: 'POST',
+            url: raiz + "houses/"+"Diego",
+            data: JSON.stringify($scope.houseWrapper)
+        }).then(function (response){
+            if(response){
+                console.log(response);
+                location.reload();
+            }else{
+                console.log("Sin permisos");
+            }
+        })
+	}
+	
+	// Consultar casas
+	$scope.consultarCasas = () => {
+		$http({
+            method: 'GET',
+            url: raiz + "houses",
+            data: {}
+        }).then(function (response){
+            if(response){
+                console.log(response);
+                $scope.casas = response.data;
+            }else{
+                console.log("Sin permisos");
+            }
+        })
+	}
+	
+	
+	// Consultar casa especifica
+	$scope.consultarCasaEspecifica = (idCasa) => {
+		$http({
+            method: 'GET',
+            url: raiz + "houses/"+JSON.stringify(idCasa),
+            data: {}
+        }).then(function (response){
+            if(response){
+                console.log(response);
+                $scope.houseWrapper = response.data;
+            }else{
+                console.log("Sin permisos");
+            }
+        })
+	}
+	
+	// Modificar casa
+	$scope.modificarCasa = () => {
+		$http({
+            method: 'GET',
+            url: raiz + "houses/"+JSON.stringify(idCasa),
+            data: {}
+        }).then(function (response){
+            if(response){
+                console.log(response);
+                $scope.houseWrapper = response.data;
+            }else{
+                console.log("Sin permisos");
+            }
+        })
+	}
 
 });
