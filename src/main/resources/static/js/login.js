178e0144041af28b0ec2269d5024369a3168104a
DiegoVelascoGH <20193tn054@utez.edu.mx>
2022-02-13 22:21:06 -0600

    [UPDATE] Front end

--- src/main/resources/static/js/login.js
+++
@@ -1,8 +0,0 @@
-const login = () => {
-    alert('Estas entrando al sistema...');
-    url = "../casas.html"
-    var link = document.createElement('a');
-        link.href = url;
-        document.body.appendChild(link);
-        link.click();
-}
